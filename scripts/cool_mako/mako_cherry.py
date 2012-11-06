b65302003e6969ed546d16b7639a8f0cc0a18cb6
Sasen Cain <sasen@mit.edu>
2012-11-05 19:08:44 -0500

    ENH: realtime run button-logic improvements ... added rtDone() in buttonlib.py to update progress to the next step

--- scripts/cool_mako/mako_cherry.py
+++ scripts/cool_mako/mako_cherry.py
@@ -109,11 +109,6 @@ class MakoRoot:
         Don't do this until we're done with this action!
         TODO: figure out when RT runs and psychopy stuff is done
         """
-        ## for realtime runs, update to the next run (step)
-        node = bt.btn_node(bid, self.json)
-        if hasattr(node, 'done'):
-            if node['done']:
-                bid = lib.get_node(bt.sib_node(bid, self.json, -1), 'id')
         print "new progress will be",bid
         bt.setProgress(bid, bt.get_visit(bid,self.json))
         return
@@ -144,9 +139,7 @@ class MakoRoot:
                 activeData = lib.load_json(activeFile)
                 print "activeData's length is:", len(activeData['data'])
                 if len(activeData['data']) > 150:
-                    lib.set_here(node,'done',True)
-                    lib.set_here(node,'disabled','True')  ## could do this better
-                    self.updateProgress(node['id'])
+                    bt.rtDone(self.json, node['id'])
                 else:
                     lib.set_here(bt.sib_node(node['id'], self.json, 3), "disabled", False)                
             ## maybe enable redo? disable this run, enable next run,
